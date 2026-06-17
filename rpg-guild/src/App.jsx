a148b359cf23ad5e36389434c759984fa00726ec
nicolasneves34 <nicolasneves622@gmail.com>
2026-06-17 15:11:46 -0300

    guilda rpg

---
+++ rpg-guild/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Route ,Routes } from 'react-router-dom'
+
+import { Navbar } from './components/navbar'
+import { Home } from './components/home'
+import { Guilds } from './components/guild'
+import { Members } from './components/member'
+import { MemberForm } from './components/memberForm'
+import { GuildForm } from './components/guildForm'
+
+const App = () => (
+  <Router>
+    <div className="bg-[#ECEAE0] w-screen h-screen">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/guilds" element={<Guilds />} />
+        <Route path="/members" element={<Members />} />
+              <Route path="/guilds/:guildId" element={<GuildForm />} />
+              <Route path="/members/:memberId" element={<MemberForm />} />
+      </Routes>
+    </div>
+  </Router>
+)
+
+export default App;
---
+++ rpg-guild/src/components/guild.jsx
@@ -0,0 +1,7 @@
+export function Guilds() {
+    return(
+        <div className="flex flex-col gap-4" p-5 text-orange-500>
+            <h1>Guildas</h1>
+        </div>
+    )
+}
