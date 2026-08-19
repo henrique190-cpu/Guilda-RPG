a161963800cf8726ee8e26d155ff72abc6b0d50f
nicolasneves34 <nicolasneves622@gmail.com>
2026-08-19 13:10:34 -0300

    ultimas atualizacoes

---
+++ rpg-guild/src/components/category.jsx
@@ -0,0 +1,4 @@
+import { useNavigate } from "react-router-dom";
+import { GuildForm } from "./guildForm";
+import { useState , useEffect} from "react";
+import requester from "../axios";
---
+++ rpg-guild/src/components/categoryForm.jsx
@@ -0,0 +1,4 @@
+import { useParams } from "react-router-dom";
+import { useState , useEffect } from "react";
+
+import requester from "../axios";
